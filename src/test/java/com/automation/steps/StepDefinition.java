c04a1070c5286dcfc3ec174d71d8cccc8b071314
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-12 10:24:03 +0300

    Update StepDefinition.java

--- src/test/java/com/automation/steps/StepDefinition.java
+++ src/test/java/com/automation/steps/StepDefinition.java
@@ -7,22 +7,19 @@ import io.cucumber.java.en.When;
 public class StepDefinition {
     @Given("user open the website")
     public void user_open_the_website() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        
     }
+
     @Given("verify user is on login page")
     public void verify_user_is_on_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @When("user enters valid credentials")
     public void user_enters_valid_credentials() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
     @Then("verify user is on home page")
     public void verify_user_is_on_home_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 }
